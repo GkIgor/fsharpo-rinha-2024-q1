692d784346efe3f0266b7a5680fc9e81ead08c19
GK.irxzin <GK.irxzin@DESKTOP-Q94KDVO>
2024-02-23 18:06:25 -0300

    Adicionar arquivos de projeto.

---
+++ Rinha/Controllers/TransationsController.fs
@@ -0,0 +1,20 @@
+﻿namespace Rinha.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open Rinha
+
+[<ApiController>]
+[<Route("[clientes]")>]
+type TransationsController (logger : ILogger<TransationsController>) =
+    inherit ControllerBase()
+    
+    [<HttpGet("{id}/transacoes")>]
+    member _.Get() =
+        
+        
+    
